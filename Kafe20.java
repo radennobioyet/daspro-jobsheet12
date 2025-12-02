187db69ba387401d386c556b9de87a96a64972f6
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2025-12-02 22:38:37 +0700

    percobaan 1

---
+++ Kafe20.java
@@ -0,0 +1,16 @@
+public class Kafe20 {
+    public static void main(String[] args) {
+        menu();
+    }
+    public static void menu() {
+        System.out.println("==== MENU RESTO CAFE ====:");
+        System.out.println("1. Kopi hitam - Rp 15.000");
+        System.out.println("2. Cappucino - Rp 20.000");
+        System.out.println("3. Latte - Rp 22.000");
+        System.out.println("4. Teh tarik - Rp 12.000");
+        System.out.println("5. Roti Bakar - Rp 10.000");
+        System.out.println("6. Mie Goreng - Rp 18.000");
+        System.out.println("============================");
+        System.out.println("Silahkan pilih menu yang diinginkan:");
+    }
+}
