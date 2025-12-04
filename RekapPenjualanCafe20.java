ee66c7e307731c12ba100964ef4ab04796ef7e6f
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2025-12-04 19:57:54 +0700

    modifikasi soal

--- RekapPenjualanCafe20.java
+++ RekapPenjualanCafe20.java
@@ -1,35 +1,63 @@
-public class RekapPenjualanCafe20 {
-    static String[] menu = { "Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan" };
-    static int[][] penjualan = {
-        { 20, 20, 20, 60, 60, 60, 10 },
-        { 30, 9, 10, 19, 15, 20, 25 },
-        { 5, 9, 20, 25, 10, 5, 45 },
-        { 50, 8, 17, 18, 10, 30, 6 },
-        { 15, 10, 16, 15, 10, 10, 55 }
-    };
+import java.util.Scanner;
 
-    // Menampilkan data penjualan dalam bentuk tabel
+public class RekapPenjualanCafe20 {
+    static String[] menu;
+    static int[][] penjualan;
+
+    // Input jumlah menu, nama menu, dan data penjualan
+    public static void inputData() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah menu: ");
+        int jumlahMenu = input.nextInt();
+        input.nextLine(); // buang newline
+
+        System.out.print("Masukkan jumlah hari penjualan: ");
+        int jumlahHari = input.nextInt();
+        input.nextLine(); // buang newline
+
+        menu = new String[jumlahMenu];
+        penjualan = new int[jumlahMenu][jumlahHari];
+
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print("Nama menu ke-" + (i + 1) + ": ");
+            menu[i] = input.nextLine();
+        }
+
+        for (int i = 0; i < jumlahMenu; i++) {
+            for (int j = 0; j < jumlahHari; j++) {
+                System.out.print("Penjualan " + menu[i] + " pada hari ke-" + (j + 1) + ": ");
+                penjualan[i][j] = input.nextInt();
+            }
+        }
+        input.nextLine(); // buang newline
+    }
+
+    // Tampilkan tabel penjualan
     public static void tampilData() {
-        System.out.printf("%-16s%-8s%-8s%-8s%-8s%-8s%-8s%-8s%n",
-                "Menu", "Hari1", "Hari2", "Hari3", "Hari4", "Hari5", "Hari6", "Hari7");
+        System.out.printf("%-16s", "Menu");
+        for (int j = 0; j < penjualan[0].length; j++) {
+            System.out.printf("%4s", "Hari-" + (j + 1));
+        }
+        System.out.println();
 
         for (int i = 0; i < menu.length; i++) {
             System.out.printf("%-16s", menu[i]);
-            for (int j = 0; j < 7; j++) {
-                System.out.printf("%-8d", penjualan[i][j]);
+            for (int j = 0; j < penjualan[i].length; j++) {
+                System.out.printf("%4d", penjualan[i][j]);
             }
             System.out.println();
         }
     }
 
-    // Menampilkan menu dengan total penjualan tertinggi
+    // Menu dengan total penjualan tertinggi
     public static void tampilMenuTerlaris() {
         int maxTotal = 0;
         String menuTerlaris = "";
 
         for (int i = 0; i < menu.length; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < penjualan[i].length; j++) {
                 total += penjualan[i][j];
             }
 
@@ -39,26 +67,30 @@ public class RekapPenjualanCafe20 {
             }
         }
 
-        System.out.println("Menu terlaris: " + menuTerlaris + " dengan total penjualan " + maxTotal);
+        System.out.println("Menu terlaris : " + menuTerlaris + " dengan total penjualan " + maxTotal);
     }
 
-    // Menampilkan rata-rata penjualan tiap menu
+    // Rata-rata penjualan tiap menu
     public static void tampilRataRata() {
         System.out.println("Rata-rata penjualan per menu:");
         for (int i = 0; i < menu.length; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < penjualan[i].length; j++) {
                 total += penjualan[i][j];
             }
 
-            double rata = total / 7.0;
-            System.out.println(menu[i] + " : " + rata);
+            double rata = total / (double) penjualan[i].length;
+            System.out.println(menu[i] + " = " + rata);
         }
     }
 
     public static void main(String[] args) {
+        inputData();
+        System.out.println("\n=== TABEL PENJUALAN ===");
         tampilData();
+        System.out.println("\n=== MENU TERLARIS ===");
         tampilMenuTerlaris();
+        System.out.println("\n=== RATA-RATA PENJUALAN ===");
         tampilRataRata();
     }
 }
